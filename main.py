84a939b70dd9c3a49b81baca5c7d3f8fde2e0140
Arsène Thieffry <druz320@gmail.com>
2016-02-06 18:03:05 +0100

    Réception des messages

--- main.py
+++ main.py
@@ -15,17 +15,6 @@ s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 s.bind(addr)
 
-def recv():
-    while True:
-        msg = s.recvfrom(1024)
-        if not msg: sys.exit(0)
-        print(msg[0].decode())
-
-def send(msg):
-    s.sendto(bytes(msg, "utf-8"), addr)
-
-Thread(target=recv).start()
-
 class MyWindow(Gtk.Window):
 
     def __init__(self):
@@ -35,7 +24,25 @@ class MyWindow(Gtk.Window):
         self.vbox.set_border_width(12)
         self.add(self.vbox)
 
+        self.create_receiver()
         self.create_sender()
+
+        self.nickname = "Druz"
+
+
+        Thread(target=self.recv).start()
+
+    def create_receiver(self):
+        scrolledWindow = Gtk.ScrolledWindow()
+        scrolledWindow.set_hexpand(True)
+        scrolledWindow.set_vexpand(True)
+        self.vbox.pack_start(scrolledWindow, True, True, 0)
+
+        self.textview = Gtk.TextView()
+        self.textview.set_editable(False)
+        self.textview.set_wrap_mode(Gtk.WrapMode.WORD)
+        self.textbuffer = self.textview.get_buffer()
+        scrolledWindow.add(self.textview)
 
     def create_sender(self):
         self.box = Gtk.Box(spacing=6)
@@ -50,8 +57,22 @@ class MyWindow(Gtk.Window):
 
     def on_button_clicked(self, widget):
         if self.entry.get_text():
-            send(self.entry.get_text())
+            self.send(self.nickname + " : " + self.entry.get_text())
             self.entry.set_text("")
+
+    def send(self, msg):
+        s.sendto(bytes(msg, "utf-8"), addr)
+
+    def recv(self):
+        while True:
+            msg = s.recvfrom(1024)
+            if not msg: sys.exit(0)
+            self.textbuffer.insert_at_cursor(msg[0].decode() + "\n")
+
+    def change_nickname(self, nickname):
+        if self.nickname != nickname:
+            self.send(self.nickname + " is now known as " + nickname)
+            self.nickname = nickname
 
 win = MyWindow()
 win.connect("delete-event", Gtk.main_quit)
